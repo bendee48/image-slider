07ca46f760bfdfffa15cc577ec36d9b5f5efec6d
Ben Daniels <benbanj@hotmail.com>
2023-01-05 15:00:47 +0000

    Adding Navigation Dots
    - Adding function to generate dot elements using the number of pics
    - Styling dots
    - Adding functionality to highlight current picture with dot nav

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const prevBtn = document.querySelector('.prevBtn');
 const nextBtn = document.querySelector('.nextBtn');
+const slider = document.querySelector('.image-slider');
+const navDotContainer = document.querySelector('.nav-dot-container');
 const pics = document.querySelectorAll('img');
 let counter = 0; // To keep track of a picture's place
-const slider = document.querySelector('.image-slider');
 const pic = pics[0];
+navigationDots() // Append navigation dots
+let activeDot;
+setActiveDot();
 
 nextBtn.addEventListener('click', () => {
   if (counter === pics.length-1) {
@@ -12,6 +16,10 @@ nextBtn.addEventListener('click', () => {
   counter++;
   // -pic.clientWidth * counter pushes the slider 1 picture length left 
   slider.style.transform = `translateX(${-pic.clientWidth * counter}px)`;
+  let nextDot = document.querySelector(`[data-index="${counter}"]`);
+  nextDot.classList.add('active');
+  activeDot.classList.remove('active');
+  activeDot = nextDot;
 });
 
 prevBtn.addEventListener('click', () => {
@@ -20,4 +28,22 @@ prevBtn.addEventListener('click', () => {
   }
   counter--;
   slider.style.transform = `translateX(${-pic.clientWidth * counter}px)`;
+  let nextDot = document.querySelector(`[data-index="${counter}"]`);
+  nextDot.classList.add('active');
+  activeDot.classList.remove('active');
+  activeDot = nextDot;
 });
+
+function navigationDots() {
+  for (let i = 0; i < pics.length; i++) {
+    let navDot = document.createElement('div');
+    navDot.className = 'nav-dot';
+    navDot.dataset.index = i;
+    navDotContainer.appendChild(navDot);
+  }
+}
+
+function setActiveDot() {
+  activeDot = document.querySelector(`[data-index="${counter}"]`);
+  activeDot.classList.add('active');
+}
