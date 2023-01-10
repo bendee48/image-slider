21d6e41bf428bd130243f15b18b0a5d16b9c223a
Ben Daniels <benbanj@hotmail.com>
2023-01-10 17:04:55 +0000

    Adding functionality to view pic from Nav Dot
    - Refactor setting and removing active dot in the prev and next
    functions into own functions
    - Add function to jump to selected pic via nav dots

--- script.js
+++ script.js
@@ -5,40 +5,37 @@ const navDotContainer = document.querySelector('.nav-dot-container');
 const pics = document.querySelectorAll('img');
 let counter = 0; // To keep track of a picture's place
 const pic = pics[0];
-navigationDots() // Append navigation dots
+apendNavigationDots() // Append navigation dots using number of pics
 let activeDot;
 setActiveDot();
 
 nextBtn.addEventListener('click', () => {
+  removeActiveDot();
   if (counter === pics.length-1) {
     counter = -1;
   };
   counter++;
   // -pic.clientWidth * counter pushes the slider 1 picture length left 
   slider.style.transform = `translateX(${-pic.clientWidth * counter}px)`;
-  let nextDot = document.querySelector(`[data-index="${counter}"]`);
-  nextDot.classList.add('active');
-  activeDot.classList.remove('active');
-  activeDot = nextDot;
+  setActiveDot();
 });
 
 prevBtn.addEventListener('click', () => {
+  removeActiveDot();
   if (counter <= 0) {
     counter = pics.length;
   }
   counter--;
   slider.style.transform = `translateX(${-pic.clientWidth * counter}px)`;
-  let nextDot = document.querySelector(`[data-index="${counter}"]`);
-  nextDot.classList.add('active');
-  activeDot.classList.remove('active');
-  activeDot = nextDot;
+  setActiveDot();
 });
 
-function navigationDots() {
+function apendNavigationDots() {
   for (let i = 0; i < pics.length; i++) {
     let navDot = document.createElement('div');
     navDot.className = 'nav-dot';
     navDot.dataset.index = i;
+    navDot.addEventListener('click', jumpToPicture);
     navDotContainer.appendChild(navDot);
   }
 }
@@ -46,4 +43,17 @@ function navigationDots() {
 function setActiveDot() {
   activeDot = document.querySelector(`[data-index="${counter}"]`);
   activeDot.classList.add('active');
+}
+
+function removeActiveDot() {
+  activeDot = document.querySelector(`[data-index="${counter}"]`);
+  activeDot.classList.remove('active');
+}
+
+function jumpToPicture(e) {
+  removeActiveDot();
+  const newCounter = e.target.dataset.index;
+  slider.style.transform = `translateX(${-pic.clientWidth * newCounter}px)`;
+  counter = Number(newCounter);
+  setActiveDot();
 }
