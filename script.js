fbca2e71c9c3f5ba3a6bc18814b4bc7b7896ede6
Ben Daniels <benbanj@hotmail.com>
2023-01-11 17:39:57 +0000

    Adding Auto Slide Functionality
    - Move slide left and slide right functionality into their own functions
    - Add functions to start the autoslide and reset autoslide
    - Add in reset autoslide function to existing navigational functions
    - Update comments

--- script.js
+++ script.js
@@ -5,31 +5,22 @@ const navDotContainer = document.querySelector('.nav-dot-container');
 const pics = document.querySelectorAll('img');
 let counter = 0; // To keep track of a picture's place
 const pic = pics[0];
-apendNavigationDots() // Append navigation dots using number of pics
-let activeDot;
-setActiveDot();
+apendNavigationDots()
+let activeDot; // Keep track of current active nav dot
+setActiveDot(); // Set initial nav dot to be highlighted (first pic)
+let intervalID = startAutoSlide(); // Keep interval ID for use in cancelling
 
 nextBtn.addEventListener('click', () => {
-  removeActiveDot();
-  if (counter === pics.length-1) {
-    counter = -1;
-  };
-  counter++;
-  // -pic.clientWidth * counter pushes the slider 1 picture length left 
-  slider.style.transform = `translateX(${-pic.clientWidth * counter}px)`;
-  setActiveDot();
+  resetAutoSlide();
+  slideToRight();
 });
 
 prevBtn.addEventListener('click', () => {
-  removeActiveDot();
-  if (counter <= 0) {
-    counter = pics.length;
-  }
-  counter--;
-  slider.style.transform = `translateX(${-pic.clientWidth * counter}px)`;
-  setActiveDot();
+  resetAutoSlide();
+  slideToLeft();
 });
 
+// Append nav dots dynamically using number of pictures
 function apendNavigationDots() {
   for (let i = 0; i < pics.length; i++) {
     let navDot = document.createElement('div');
@@ -51,9 +42,40 @@ function removeActiveDot() {
 }
 
 function jumpToPicture(e) {
+  resetAutoSlide();
   removeActiveDot();
-  const newCounter = e.target.dataset.index;
+  const newCounter = e.target.dataset.index; // Use index from target to update counter for translate
   slider.style.transform = `translateX(${-pic.clientWidth * newCounter}px)`;
-  counter = Number(newCounter);
+  counter = Number(newCounter); // Data attribute is string, convert to number
   setActiveDot();
+}
+
+function slideToRight() {
+  removeActiveDot();
+  if (counter === pics.length-1) {
+    counter = -1;
+  };
+  counter++;
+  // -pic.clientWidth * counter pushes the slider 1 picture length left 
+  slider.style.transform = `translateX(${-pic.clientWidth * counter}px)`;
+  setActiveDot();
+}
+
+function slideToLeft() {
+  removeActiveDot();
+  if (counter <= 0) {
+    counter = pics.length;
+  }
+  counter--;
+  slider.style.transform = `translateX(${-pic.clientWidth * counter}px)`;
+  setActiveDot();
+}
+
+function startAutoSlide() {
+  return setInterval(slideToRight, 5000);
+}
+
+function resetAutoSlide() {
+  clearInterval(intervalID);
+  intervalID = startAutoSlide();
 }
